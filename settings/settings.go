5aec9360faa49ebc22761bcadce62bbcb2f166bf
lowking <life@lowking.pro>
2023-07-04 14:18:09 +0800

    feat(settings): supports shortcut

--- settings/settings.go
+++ settings/settings.go
@@ -82,6 +82,7 @@ func (s *Settings) GenCommonSettings() *fyne.Container {
 func (s *Settings) GenFgSettings() *fyne.Container {
 	// 糖豆人进程获取设置
 	s.genGetFgPidSettingsRow()
+	// 糖豆人自动点击坐标设置
 	s.genFgAutoClickSettingsRow()
 
 	return container.NewWithoutLayout(s.fgSettingItems...)
@@ -200,8 +201,9 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	s.fgSettingItems = append(s.fgSettingItems, fgLabel)
 
 	// 选择节目按钮点击坐标文本框
+	y = lineHeight*3.5 + config.Padding
 	pSelectShowPos := app.Preferences().StringWithFallback(PSelectShowPos, "")
-	selectShowPosEntry := searchentry.NewSearchEntry("选择节目按钮的坐标: x,y")
+	selectShowPosEntry := searchentry.NewSearchEntry("x,y")
 	selectShowPosEntry.Wrapping = fyne.TextTruncate
 	s.PosSelectShow = &pSelectShowPos
 	selectShowPosEntry.Bind(binding.BindString(s.PosSelectShow))
@@ -209,10 +211,15 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	selectShowPosEntry.Resize(fyne.NewSize(160, 35))
 	selectShowPosEntry.Move(fyne.NewPos(config.Padding+fgLabel.Size().Width, y))
 	s.fgSettingItems = append(s.fgSettingItems, selectShowPosEntry)
+	selectShowPosLabel := widget.NewLabel("选择节目按钮")
+	selectShowPosLabel.Alignment = fyne.TextAlignCenter
+	selectShowPosLabel.Resize(selectShowPosEntry.Size())
+	selectShowPosLabel.Move(fyne.NewPos(selectShowPosEntry.Position().X, selectShowPosEntry.Position().Y-selectShowPosEntry.Size().Height+5))
+	s.fgSettingItems = append(s.fgSettingItems, selectShowPosLabel)
 
 	// 输入代码按钮点击坐标文本框
 	pEnterMapIdPos := app.Preferences().StringWithFallback(PEnterMapIdPos, "")
-	enterMapIdPosEntry := searchentry.NewSearchEntry("输入代码按钮的坐标: x,y")
+	enterMapIdPosEntry := searchentry.NewSearchEntry("x,y")
 	enterMapIdPosEntry.Wrapping = fyne.TextTruncate
 	s.PosEnterMapId = &pEnterMapIdPos
 	enterMapIdPosEntry.Bind(binding.BindString(s.PosEnterMapId))
@@ -220,10 +227,15 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	enterMapIdPosEntry.Resize(fyne.NewSize(160, 35))
 	enterMapIdPosEntry.Move(fyne.NewPos(config.Padding*2+fgLabel.Size().Width+selectShowPosEntry.Size().Width, y))
 	s.fgSettingItems = append(s.fgSettingItems, enterMapIdPosEntry)
+	enterMapIdPosLabel := widget.NewLabel("输入代码按钮")
+	enterMapIdPosLabel.Alignment = fyne.TextAlignCenter
+	enterMapIdPosLabel.Resize(enterMapIdPosEntry.Size())
+	enterMapIdPosLabel.Move(fyne.NewPos(enterMapIdPosEntry.Position().X, enterMapIdPosEntry.Position().Y-enterMapIdPosEntry.Size().Height+5))
+	s.fgSettingItems = append(s.fgSettingItems, enterMapIdPosLabel)
 
 	// 代码输入框点击坐标文本框
 	pCodeEntryPos := app.Preferences().StringWithFallback(PCodeEntryPos, "")
-	codeEntryPosEntry := searchentry.NewSearchEntry("代码输入框的坐标: x,y")
+	codeEntryPosEntry := searchentry.NewSearchEntry("x,y")
 	codeEntryPosEntry.Wrapping = fyne.TextTruncate
 	s.PosCodeEntry = &pCodeEntryPos
 	codeEntryPosEntry.Bind(binding.BindString(s.PosCodeEntry))
@@ -231,10 +243,15 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	codeEntryPosEntry.Resize(fyne.NewSize(160, 35))
 	codeEntryPosEntry.Move(fyne.NewPos(config.Padding*3+fgLabel.Size().Width+selectShowPosEntry.Size().Width+enterMapIdPosEntry.Size().Width, y))
 	s.fgSettingItems = append(s.fgSettingItems, codeEntryPosEntry)
+	codeEntryPosLabel := widget.NewLabel("代码输入框")
+	codeEntryPosLabel.Alignment = fyne.TextAlignCenter
+	codeEntryPosLabel.Resize(codeEntryPosEntry.Size())
+	codeEntryPosLabel.Move(fyne.NewPos(codeEntryPosEntry.Position().X, codeEntryPosEntry.Position().Y-codeEntryPosEntry.Size().Height+5))
+	s.fgSettingItems = append(s.fgSettingItems, codeEntryPosLabel)
 
 	// 确认按钮点击坐标文本框
 	pConfirmBtnPos := app.Preferences().StringWithFallback(PConfirmBtnPos, "")
-	confirmBtnPosEntry := searchentry.NewSearchEntry("确认按钮的坐标: x,y")
+	confirmBtnPosEntry := searchentry.NewSearchEntry("x,y")
 	confirmBtnPosEntry.Wrapping = fyne.TextTruncate
 	s.PosConfirmBtn = &pConfirmBtnPos
 	confirmBtnPosEntry.Bind(binding.BindString(s.PosConfirmBtn))
@@ -242,6 +259,11 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	confirmBtnPosEntry.Resize(fyne.NewSize(160, 35))
 	confirmBtnPosEntry.Move(fyne.NewPos(config.Padding*4+fgLabel.Size().Width+selectShowPosEntry.Size().Width+enterMapIdPosEntry.Size().Width+codeEntryPosEntry.Size().Width, y))
 	s.fgSettingItems = append(s.fgSettingItems, confirmBtnPosEntry)
+	confirmBtnPosLabel := widget.NewLabel("确认按钮")
+	confirmBtnPosLabel.Alignment = fyne.TextAlignCenter
+	confirmBtnPosLabel.Resize(confirmBtnPosEntry.Size())
+	confirmBtnPosLabel.Move(fyne.NewPos(confirmBtnPosEntry.Position().X, confirmBtnPosEntry.Position().Y-confirmBtnPosEntry.Size().Height+5))
+	s.fgSettingItems = append(s.fgSettingItems, confirmBtnPosLabel)
 	logger.Debugf("Binding Select Show: %s EnterMapId: %s CodeEntry: %s ConfirmBtn: %s", pSelectShowPos, pEnterMapIdPos, pCodeEntryPos, pConfirmBtnPos)
 
 	// 测试点击按钮
@@ -311,4 +333,74 @@ func (s *Settings) genFgAutoClickSettingsRow() {
 	confirmBtnPosEntry.OnCursorChanged = func() {
 		fyne.CurrentApp().Preferences().SetString(PConfirmBtnPos, confirmBtnPosEntry.Text)
 	}
+
+	// 快捷键设置
+	// 根据给定参数生成连线
+	objects := []fyne.CanvasObject{selectShowPosEntry, enterMapIdPosEntry, codeEntryPosEntry, confirmBtnPosEntry}
+	s.genEntryWithLink(objects[1:], "例：ctrl+alt+n，回车保存", 150, 35, func(str string) {
+		keys := strings.Split(str, "+")
+		if len(keys) < 3 {
+			dialog.ShowInformation("提示", "快捷键必须至少3个按键", *s.Window)
+			return
+		}
+	}, 2)
+	s.genEntryWithLink(objects, "例：ctrl+alt+p，回车保存", 150, 35, func(str string) {
+		keys := strings.Split(str, "+")
+		if len(keys) < 3 {
+			dialog.ShowInformation("提示", "快捷键必须至少3个按键", *s.Window)
+			return
+		}
+	}, 1)
+}
+
+func (s *Settings) genEntryWithLink(objects []fyne.CanvasObject, placeHolder string, width float32, height float32, onSubmit func(s string), lineNo float32) {
+	// 根据objects计算连线起点坐标
+	linkStartEndpoint := canvas.NewRectangle(config.ShadowColor)
+	linkStartEndpoint.Resize(fyne.NewSize(5, height*lineNo))
+	linkStartEndpoint.Move(fyne.NewPos(objects[0].Position().X+objects[0].Size().Width/2, objects[0].Position().Y+objects[0].Size().Height))
+	s.fgSettingItems = append(s.fgSettingItems, linkStartEndpoint)
+	linkEndEndpoint := canvas.NewRectangle(config.ShadowColor)
+	linkEndEndpoint.Resize(fyne.NewSize(5, height*lineNo))
+	linkEndEndpoint.Move(fyne.NewPos(objects[len(objects)-1].Position().X+objects[len(objects)-1].Size().Width/2, objects[len(objects)-1].Position().Y+objects[len(objects)-1].Size().Height))
+	s.fgSettingItems = append(s.fgSettingItems, linkEndEndpoint)
+
+	link := canvas.NewRectangle(config.ShadowColor)
+	// var linkWidth float32
+	// for i := 0; i < len(objects); i++ {
+	// 	switch i {
+	// 	case 0:
+	// 		linkWidth += objects[i].Size().Width/2
+	// 	case len(objects) - 1:
+	// 		linkWidth += objects[i].Size().Width/2 + config.Padding
+	// 	default:
+	// 		linkWidth += objects[i].Size().Width
+	// 	}
+	// }
+	link.Resize(fyne.NewSize(linkEndEndpoint.Position().X-linkStartEndpoint.Position().X+linkEndEndpoint.Size().Width, 5))
+	link.Move(fyne.NewPos(linkStartEndpoint.Position().X, linkStartEndpoint.Position().Y+linkStartEndpoint.Size().Height))
+	s.fgSettingItems = append(s.fgSettingItems, link)
+
+	// 生成文本框
+	entry := searchentry.NewSearchEntry(placeHolder)
+	entry.Wrapping = fyne.TextTruncate
+	entry.Resize(fyne.NewSize(width, height))
+	entry.Move(fyne.NewPos(link.Position().X+link.Size().Width/2-width/2, link.Position().Y+link.Size().Height-height/2-2.5))
+	entry.OnSubmitted = onSubmit
+	entry.OnCursorChanged = func() {
+		linkStartEndpoint.FillColor = config.ShadowColor
+		linkEndEndpoint.FillColor = config.ShadowColor
+		link.FillColor = config.ShadowColor
+		linkStartEndpoint.Refresh()
+		linkEndEndpoint.Refresh()
+		link.Refresh()
+	}
+	entry.OnTapped = func(event *fyne.PointEvent) {
+		linkStartEndpoint.FillColor = config.AccentColor
+		linkEndEndpoint.FillColor = config.AccentColor
+		link.FillColor = config.AccentColor
+		linkStartEndpoint.Refresh()
+		linkEndEndpoint.Refresh()
+		link.Refresh()
+	}
+	s.fgSettingItems = append(s.fgSettingItems, entry)
 }
