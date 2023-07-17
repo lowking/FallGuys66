c7045495ce500638bff515096ea0d21d4e4c8f7c
lowking <life@lowking.pro>
2023-07-17 14:15:31 +0800

    feat: save log

--- config/ui.go
+++ config/ui.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fyne.io/fyne/v2"
 	"image/color"
+	"os"
+	"path/filepath"
 )
 
 var AppName = "糖豆人直播助手：大周定制版"
@@ -12,6 +14,20 @@ var Padding = float32(10)
 var ToolbarPaddingTop = Padding + 10
 var ToolbarPaddingLeft = float32(120)
 var RemarkText = `### PS, 💖 Power with love.`
+var UserConfigDir string
+
+func init() {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+	configDir = filepath.Join(configDir, "fallguys66")
+	err = os.MkdirAll(configDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+	UserConfigDir = configDir
+}
 
 var ShadowColor = color.RGBA{
 	R: 66,
--- live/douyu/lib/logger/print.go
+++ live/douyu/lib/logger/print.go
@@ -1,45 +1,61 @@
 package logger
 
 import (
+	"FallGuys66/config"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-var debugLogger = log.New(os.Stdout, "[DEBUG]", log.LstdFlags)
-var infoLogger = log.New(os.Stdout, "[INFO]", log.LstdFlags)
-var warnLogger = log.New(os.Stderr, "[WARN]", log.LstdFlags)
-var errorLogger = log.New(os.Stderr, "[ERROR]", log.LstdFlags)
+var debugLogger *log.Logger
+var infoLogger *log.Logger
+var warnLogger *log.Logger
+var errorLogger *log.Logger
+
+func init() {
+	logFilePath := filepath.Join(config.UserConfigDir, "FallGuys66.log")
+	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open logger file:", err)
+	}
+	debugLogger = log.New(io.MultiWriter(file, os.Stdout), "[DEBUG] ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLogger = log.New(io.MultiWriter(file, os.Stdout), "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
+	warnLogger = log.New(io.MultiWriter(file, os.Stderr), "[WARN] ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLogger = log.New(io.MultiWriter(file, os.Stderr), "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func Errorf(format string, v ...interface{}) {
-	errorLogger.Printf(format, v...)
+	errorLogger.Output(2, fmt.Sprintf(format, v...))
 }
-func Error(v ...interface{}) {
-	errorLogger.Println(v)
+func Error(v interface{}) {
+	errorLogger.Output(2, fmt.Sprintf("%v", v))
 }
 func Err(err error) {
-	errorLogger.Println(err.Error())
+	errorLogger.Output(2, err.Error())
 }
 func Warnf(format string, v ...interface{}) {
-	warnLogger.Printf(format, v...)
+	warnLogger.Output(2, fmt.Sprintf(format, v...))
 }
-func Warn(v ...interface{}) {
-	warnLogger.Println(v)
+func Warn(v interface{}) {
+	warnLogger.Output(2, fmt.Sprintf("%v", v))
 }
 func Infof(format string, v ...interface{}) {
-	infoLogger.Printf(format, v...)
+	infoLogger.Output(2, fmt.Sprintf(format, v...))
 }
-func Info(v ...interface{}) {
-	infoLogger.Println(v...)
+func Info(v interface{}) {
+	infoLogger.Output(2, fmt.Sprintf("%v", v))
 }
 func Debugf(format string, v ...interface{}) {
-	debugLogger.Printf(format, v...)
+	debugLogger.Output(2, fmt.Sprintf(format, v...))
 }
-func Debug(v ...interface{}) {
-	debugLogger.Println(v)
+func Debug(v interface{}) {
+	debugLogger.Output(2, fmt.Sprintf("%v", v))
 }
 func Deb(err error) {
-	debugLogger.Println(err.Error())
+	debugLogger.Output(2, err.Error())
 }
 func ShowJson(format string, v ...interface{}) {
 	var vt []any
@@ -49,5 +65,5 @@ func ShowJson(format string, v ...interface{}) {
 			vt = append(vt, string(b))
 		}
 	}
-	debugLogger.Printf(format, vt...)
+	debugLogger.Output(2, fmt.Sprintf(format, vt...))
 }
